d0e4378b9997814209b5a6483c9238b1fe7ab878
Hugo Matheus <hugo_matheuss@outlook.com>
2025-04-15 14:00:54 -0300

    feat: client policy

---
+++ src/app/Policies/ClientePolicy.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+use Illuminate\Auth\Access\HandlesAuthorization;
+
+class ClientePolicy
+{
+    use HandlesAuthorization;
+    
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function viewAny(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    public function create(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    public function update(User $user): bool
+    {
+        return $user->is_admin;
+    }
+
+    public function delete(User $user): bool
+    {
+        return $user->is_admin;
+    }
+}
--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 // use Illuminate\Support\Facades\Gate;
 
 use App\Models\Categoria;
+use App\Models\Cliente;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -16,6 +17,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Categoria::class => \App\Policies\CategoriaPolicy::class,
+        Cliente::class => \App\Policies\ClientePolicy::class,
     ];
 
     /**
