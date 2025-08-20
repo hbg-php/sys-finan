bdeb93f875634b3cc85298033b893e28e53a0232
Hugo Matheus <hugomatheus650@gmail.com>
2025-08-20 10:09:25 -0300

    chore: adding laravel rector rules

--- src/rector.php
+++ src/rector.php
@@ -3,7 +3,9 @@
 declare(strict_types=1);
 
 use Rector\Config\RectorConfig;
-use Rector\Php83\Rector\ClassMethod\AddOverrideAttributeToOverriddenMethodsRector;
+use Rector\Set\ValueObject\LevelSetList;
+use Rector\Set\ValueObject\SetList;
+use RectorLaravel\Set\LaravelSetList;
 
 return RectorConfig::configure()
     ->withPaths([
@@ -13,15 +15,15 @@ return RectorConfig::configure()
         __DIR__.'/database',
         __DIR__.'/public',
     ])
-    ->withSkip([
-        AddOverrideAttributeToOverriddenMethodsRector::class,
+    ->withSets([
+        LevelSetList::UP_TO_PHP_82,
+        SetList::CODE_QUALITY,
+        SetList::DEAD_CODE,
+        SetList::NAMING,
+        SetList::EARLY_RETURN,
+        SetList::TYPE_DECLARATION,
+        LaravelSetList::LARAVEL_120,
+        LaravelSetList::LARAVEL_CODE_QUALITY,
     ])
-    ->withPreparedSets(
-        deadCode: true,
-        codeQuality: true,
-        typeDeclarations: true,
-        privatization: true,
-        earlyReturn: true,
-        strictBooleans: true,
-    )
-    ->withPhpSets();
+    ->withImportNames(removeUnusedImports: true)
+    ->withPhpSets(php84: true);
